7973c69e5322766c6bb50c0d01ba6e2db53d569e
PolynomialD <steven.grindey@gmail.com>
2018-07-06 10:04:09 +0100

    Adding trees

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const ads = require('../data/ads.json')
 const animals = require('../data/animals.json')
 const stuff = require('../data/stuff.json')
-
+const trees = require('../data/stuff.json')
 /* 
  * localhost:3000
  * localhost:3000/
@@ -14,9 +14,7 @@ router.get('/', (req, res, next) => {
   res.render('pages/home', ads)
 })
 
-/* 
- * localhost:3000/dogs
- */
+/* localhost:3000/dogs */
 router.get('/dogs', (req, res) => {
   res.render('pages/animals', animals.dogs)
 })
@@ -27,6 +25,10 @@ router.get('/cats', (req, res) => {
 
 router.get('/buildings', (req, res) => {
   res.render('pages/stuff', { data: stuff.buildings })
+})
+
+router.get('/trees', (req, res) => {
+  res.render('pages/trees', { data: trees.weed })
 })
 
 module.exports = router
